1364123493d067a82b874753150e1c621b88fdd0
PCHIII <treyhelmer@gmail.com>
2019-07-12 12:28:47 -0400

    a lot of layout changes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,41 +4,50 @@
 // add for placeholder (form) user to create a new sport button, that adds to the search array.
 
 
+$(document).ready(function() {
 
 
 
 var sports = ["Volleyball", "Hockey", "Football", "Nascar", "Baseball",  "Boxing", "Gymnastics", "Golf", "Tennis", "Basketball", "Surfing", "Bowling"];
 
 function displayGif() {
-    var Osport = $(this).attr("data-Osport");
+    var oSport = $(this).attr("data-oSport");
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=funny+" + Osport + "&api_key=6Mf8kelUSBecuyyorodHx74jiGumtjs7&limit=10"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=funny+" + oSport + "&api_key=6Mf8kelUSBecuyyorodHx74jiGumtjs7&limit=10"
 
     $.ajax({
         url: queryURL,
         method: "GET"
 
     }).then(function (response) {
-        // console.log(response);
+        console.log(response);
 
-        // create variable to grab data from api
+        
+        // storing the data from the AJAX request in the results variable
         var results = response.data;
 
+// loop thru data
         // grab stills in pulled data
-
+ // Setting the attributes of the image
+//  create div tag and img tag to store
         for (var i = 0; i <= results.length; i++) {
             var gifDiv = $("<div>").attr("class", "gifDiv float-md-left");
-            var Image = $("<img>");
-            Image.attr("class", "img-fluid gif");
-            Image.attr("src", results[i].images.fixed_height_still.url);
-            Image.attr("data-still", results[i].images.fixed_height_still.url);
-            Image.attr("data-animate", results[i].images.fixed_height.url);
-            Image.attr("data-state", "still");
+            var image = $("<img>");
+            image.attr("class", "img-fluid gif");
+            image.attr("src", results[i].images.fixed_height_still.url);
+            image.attr("data-still", results[i].images.fixed_height_still.url);
+            image.attr("data-animate", results[i].images.fixed_height.url);
+            image.attr("data-state", "still");
+           
+            // image.attr("display","block");
+
 // get rating from string
+// create p tag to store rating
+// Appending the images and rating to the html container.
             var ratingPG = $("<p>")
             ratingPG.text(" rating: " + results[i].rating)
             ratingPG.attr("class", "rating");
-            gifDiv.append(Image);
+            gifDiv.append(image);
             gifDiv.append(ratingPG);
             $(".gif-container").prepend(gifDiv);
         }
@@ -58,7 +67,8 @@ $(document).on("click", ".gif", function() {
     }
 
 });
-// create a function that append newly create buttons, create buttons on click and add them to Osport
+// create a function that create buttons from array and append new to var sports array
+// Appending the buttons to the html container
 function showButtons() {
 
     $("#gif-buttons").empty();
@@ -66,23 +76,33 @@ function showButtons() {
     for (var i = 0; i < sports.length; i++) {
         var gifButton = $("<button>");
         gifButton.attr("type", "button")
-        gifButton.attr("class", "btn btn-info m-1 Osport")
+        gifButton.attr("class", "btn btn-info m-1 oSport")
         gifButton.text(sports[i])
-        gifButton.attr("data-Osport", sports[i])
+        gifButton.attr("data-oSport", sports[i])
         gifButton.attr("id", "gif-button");
         $("#gif-buttons").append(gifButton);
     }
 }
-//  click button to add a created button and add to json search, post to page.
+
 // event.preventDefault
 $("#add-gif").on("click", function(e) {
     e.preventDefault();
-    Osport = $("#gif-input").val();
-    sports.push(Osport);
+    oSport = $("#gif-input").val().trim();
+    sports.push(oSport);
     showButtons();
     // console.log(e)
+
+    
+// $(".reset").click(function() {
+//     $("#add-gif").trigger('reset'); 
+
+//     });
+
+
 });
 
-$(document).on("click", ".Osport", displayGif);
+$(document).on("click", ".oSport", displayGif);
 
 showButtons();
+
+});
